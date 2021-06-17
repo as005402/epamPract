25e74c3bec79697f37305e612760c92d9cd68fbf
Alina rdjyfjggohoh <as005406@g.bstu.by>
2021-06-17 10:32:02 +0300

    a little bit better (i guess?)

--- epamPract/Program.cs
+++ epamPract/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -19,21 +18,22 @@ namespace epamPract
                 using (StreamReader sr = new StreamReader(obj.fileName, System.Text.Encoding.Default))
                 {
                     string line;
-                    string pattern = @"\W+|(\d+,\d+)|(\d+\.\d+)|(\w+-\w+)";
+                    string pattern1 = @"\W+|(\w+-\w+)";         //only words that doesn't contain any numbers (letters only)
+                    string pattern2 = @"(\W+)|(\d+,\d+)|(\d+\.\d+)";        //numbers only (including floating point numbers)
+
                     obj.fileSize = sr.BaseStream.Length;
 
                     while ((line = sr.ReadLine()) != null)
                     {
                         obj.linesCount++;
+                        obj.punctuation += Regex.Matches(line, @"\.|,|:|;|'|""").Count();  //specified in the task --> "количество знаков препинания (.,:;'")"
+                        obj.digitsCount += Regex.Matches(line, @"\d").Count();
 
-                        obj.punctuation += Regex.Matches(line, @"\.|,|:|;|'|""").Count();
-
-                        var someWords = Regex.Split(line, pattern, RegexOptions.IgnoreCase).Where(a =>
+                        var someWords = Regex.Split(line, pattern1, RegexOptions.IgnoreCase).Where(a =>
                         {
                             foreach (char z in a) if (!char.IsLetter(z) && z != '-') return false;
                             return true;
                         }).Where(z => z != "");
-
                         foreach (string z in someWords)
                         {
                             if (!obj.words.ContainsKey(z)) obj.words.Add(z, 1);
@@ -41,23 +41,22 @@ namespace epamPract
                             if (z.Contains('-')) obj.wordsWithHyphen++;
                         }
                         obj.wordsCount += someWords.Count();
-
+                        
                         foreach (char z in line)
                         {
                             if (char.IsLetter(z) && !obj.letters.ContainsKey(z)) obj.letters.Add(z, 1);
                             else if (char.IsLetter(z)) obj.letters[z]++;
                         }
 
-                        obj.digitsCount += line.Where(a => char.IsDigit(a)).Count();
-                        obj.numbersCount += Regex.Split(line, pattern, RegexOptions.IgnoreCase).Where(a =>
+                        obj.numbersCount += Regex.Split(line, pattern2, RegexOptions.IgnoreCase).Where(a =>
                         {
                             foreach (char z in a) if (!char.IsDigit(z) && z != '.' && z != ',') return false;
                             return true;
                         }).Where(z => z != "").Count();
 
                     }
-
                 }
+
             }catch (Exception e)
             {
                 Console.WriteLine($"Error: {e.Message}");
@@ -80,26 +79,12 @@ namespace epamPract
             try
             {
                 File.WriteAllText($"{obj.fileName}.json", json);
+                Console.WriteLine(@$"Resulting JSON file can be found in the project folder (<project name>\bin\Debug\net5.0\{obj.fileName}.json)");
             } catch (Exception e)
             {
                 Console.WriteLine($"Oopsie smth went wrong\n{e.Message}");
             }
         }
         
-    }
-    class jsonObj
-    {
-        public string fileName { get; set; }
-        public long fileSize { get; set; } = 0;
-        public int lettersCount { get; set; } = 0;
-        public Dictionary<char, int> letters { get; set; } = new Dictionary<char, int>();
-        public int wordsCount { get; set; } = 0;
-        public Dictionary<string, int> words { get; set; } = new Dictionary<string, int>();
-        public int linesCount { get; set; } = 0;
-        public int digitsCount { get; set; } = 0;
-        public int numbersCount { get; set; } = 0;
-        public string longestWord { get; set; }
-        public int wordsWithHyphen { get; set; } = 0;
-        public int punctuation { get; set; } = 0;
     }
 }
---
+++ epamPract/jsonObj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace epamPract
+{
+    class jsonObj
+    {
+        public string fileName { get; set; }
+        public long fileSize { get; set; } = 0;
+        public int lettersCount { get; set; } = 0;
+        public Dictionary<char, int> letters { get; set; } = new Dictionary<char, int>();
+        public int wordsCount { get; set; } = 0;
+        public Dictionary<string, int> words { get; set; } = new Dictionary<string, int>();
+        public int linesCount { get; set; } = 0;
+        public int digitsCount { get; set; } = 0;
+        public int numbersCount { get; set; } = 0;
+        public string longestWord { get; set; }
+        public int wordsWithHyphen { get; set; } = 0;
+        public int punctuation { get; set; } = 0;
+    }
+}
